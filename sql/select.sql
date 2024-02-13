2d33965114cf0c1bf2c00fc83a9ce9e97c3bb14a
Loay Halawani <loayhalawani0@gmail.com>
2024-02-13 19:13:29 +0200

    Update select.sql

--- sql/select.sql
+++ sql/select.sql
@@ -5,17 +5,11 @@ AND C.ID IN (SELECT C.ID
 			 FROM HQ H 
 			 WHERE B.HeadquarterID = H.ID AND C.HeadquarterID = H.ID);
 
-
-
-
 SELECT FirstName, MiddleName, LastName
 FROM Employee
 WHERE Amount IN (SELECT E.Amount 
 				 FROM Employee E, Company C
 				 WHERE E.Amount > 700 AND E.CompanyID = C.ID AND C.ID LIKE '%01%');
-
-
-
 
 SELECT S.ID AS StorageID, S.CurrentCapacity, C.ID AS CompanyID
 FROM Storage S, Branch B, Company C
@@ -24,17 +18,11 @@ AND C.ID IN (SELECT C.ID
 			 FROM HQ H
 			 WHERE B.HeadquarterID = H.ID AND C.HeadquarterID = H.ID);
 
-
-
-
 SELECT C.FirstName, C.LastName, 1.15 * B.Amount AS Fine, B.Currency
 FROM Consumer C, Bill B, DeliversTo D
 WHERE (SELECT DATEDIFF(day, B.PaymentDate , D.Date) as DATEDIFF
 	   FROM BILL B
 	   HAVING DATEDIFF > 30);
-
-
-
 
 SELECT (COUNT(*) * 100/ ((SELECT COUNT(*) 
 						  FROM Makes) + (SELECT COUNT(*)
@@ -43,23 +31,14 @@ FROM Makes
 WHERE SupplierID IS NULL
 GROUP BY SupplierID;
 
-
-
-
 SELECT PlateNb, FuelType
 FROM Truck
 WHERE Capacity > 40
 GROUP BY PlateNb, FuelType;
 
-
-
-
 SELECT COUNT(M.ContractID) AS COUNT
 FROM Makes M, Contract C
 WHERE M.ConsumerID IS NULL AND M.ContractID = C.ID AND SignatureDate LIKE '___JAN___';
-
-
-
 
 SELECT R.Name, R.Relationship, R.PhoneNb, C.ID
 FROM Employee E, Relative R, Company C
@@ -68,18 +47,12 @@ AND C.ID IN (SELECT C.ID
 			 FROM Company C, Employee E
 			 WHERE C.ID = E.CompanyID AND E.ID = 'emp-0001');
 
-
-
-
 SELECT ID, FirstName, LastName
 FROM Employee
 WHERE NOT EXISTS (SELECT *
 				  FROM Relative
 				  WHERE ID = EmployeeID)
 ORDER BY FirstName;
-
-
-
 
 SELECT E.ID, E.FirstName, E.LastName, C.ID
 FROM Employee E, Company C
